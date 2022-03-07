68ecda9a57ff1433cd2b862cc1b43ee288fc301f
Lori McCurry <lori.mccurry@gmail.com>
2022-03-07 15:09:49 -0600

    modularize schema with gql tools

---
+++ products/product.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    id: 'redshoe',
+    description: 'Red Shoes',
+    price: 42.12,
+  },
+  {
+    id: 'bluejean',
+    description: 'Blue Jeans',
+    price: 55.55,
+  },
+];
